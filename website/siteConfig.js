b7162a91b50d47ef07f2df29894a37a86c25f98f
Kristin Haberkamp <72215173+khaberkamp@users.noreply.github.com>
2020-10-02 15:24:45 -0400

    Populating initial customizations

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Kristin Haberkamp', // Title for your website.
+    tagline: 'Technical Leader, Scrum Master, and Accessibilty Evangelist',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://khaberkamp.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'khaberkamp',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'kristin-haberkamp-4151a570',
         twitter: '',
-        github: '',
+        github: 'khaberkamp',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
